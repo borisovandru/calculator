da9b794b6010a42a9d736d8cf651468f1a977d24
boris <borisov21011986@rambler.ru>
2021-06-10 00:47:21 +0400

    Переделываю калькулятор

--- app/src/main/java/com/android/calculator/ui/CalculatorPresenter.java
+++ app/src/main/java/com/android/calculator/ui/CalculatorPresenter.java
@@ -6,26 +6,231 @@ import com.android.calculator.domain.Operation;
 public class CalculatorPresenter {
 
     private final CalculatorView view;
-
     private final Calculator calculator;
+    private int argumentOne = 0;
+    private int argumentTwo = 0;
+    private boolean isFirstArgument = true;
+    private Operation operator = null;
 
-    private double argOne = 0;
-    private double argTwo = 0;
 
     public CalculatorPresenter(CalculatorView view, Calculator calculator) {
         this.view = view;
         this.calculator = calculator;
+
+        publishArgument();
     }
 
+    public void onButtonSevenClicked() {
+        computeArg(7);
+
+        publishArgument();
+    }
+
+    public void onButtonEightClicked() {
+        computeArg(8);
+
+        publishArgument();
+    }
+
+    public void onButtonNineClicked() {
+        computeArg(9);
+
+        publishArgument();
+    }
+
+    public void onButtonDivideClicked() {
+        isFirstArgument = false;
+
+        if (operator != null) {
+
+            int result = calculator.performBinaryOperator(argumentOne, argumentTwo, operator);
+
+            view.showResult(String.valueOf(result));
+
+            argumentOne = result;
+            argumentTwo = 0;
+
+        }
+
+        operator = Operation.Divide;
+        operator = Operation.Multiply;
+    }
+
+    private void publishArgument() {
+
+        if (isFirstArgument) {
+            view.showResult(String.valueOf(argumentOne));
+        } else {
+            view.showResult(String.valueOf(argumentTwo));
+        }
+    }
+
+    private void computeArg(int divide) {
+        if (isFirstArgument) {
+            argumentOne = argumentOne * 10 + divide;
+        } else {
+            argumentTwo = argumentTwo * 10 + divide;
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
+    /*public void onButtonSevenClicked() {
+        computeArg(7);
+
+        publishArgument();
+    }*/
+
+   /* public void onButtonEightClicked() {
+
+        computeArg(8);
+
+        publishArgument();
+    }
+
+    public void onButtonNineClicked() {
+
+        computeArg(9);
+
+        publishArgument();
+    }
+
+    public void onButtonDivideClicked() {
+
+        computeArg(Divide);
+
+        publishArgument();
+    }
+
+    public void onButtonFourClicked() {
+
+        computeArg(4);
+
+        publishArgument();
+    }
+
+    public void onButtonFiveClicked() {
+
+        computeArg(5);
+
+        publishArgument();
+    }
+
+    public void onButtonSixClicked() {
+
+        computeArg(6);
+
+        publishArgument();
+    }
+
+    public void onButtonMultiplyClicked() {
+
+        computeArg(Multiply);
+
+        publishArgument();
+    }
 
     public void onButtonOneClicked() {
-        argOne =1;
+
+        computeArg(1);
+
+        publishArgument();
     }
 
     public void onButtonTwoClicked() {
-        argTwo =2;
 
-        view.getSomeText();
+        computeArg(2);
+
+        publishArgument();
+    }
+
+    public void onButtonThreeClicked() {
+
+        computeArg(3);
+
+        publishArgument();
+    }
+
+    public void onButtonSubtractClicked() {
+
+        computeArg(Subtract);
+
+        publishArgument();
+    }
+
+    public void onButtonDotClicked() {
+
+        int dot = 0;
+        computeArg(dot);
+
+        publishArgument();
+    }
+
+    public void onButtonZeroClicked() {
+
+        computeArg(0);
+
+        publishArgument();
+    }
+
+    public void onButtonEqualClicked() {
+
+        computeArg(Equal);
+
+        publishArgument();
+    }
+
+    public void onButtonAddClicked() {
+
+        computeArg(Add);
+
+        publishArgument();
+    }
+
+    public void onButtonClearClicked() {
+
+        computeArg(Clear);
+
+        publishArgument();
+    }*/
+
+    /*private void publishArgument() {
+
+        if (isFirstArgument) {
+            view.showResult(String.valueOf(argumentOne));
+        } else {
+            view.showResult(String.valueOf(argumentTwo));
+        }
+    }
+
+    private void computeArg(int addition) {
+        if (isFirstArgument) {
+            argumentOne = argumentOne * 10 + addition;
+        } else {
+            argumentTwo = argumentTwo * 10 + addition;
+        }
+
+    }*/
+    /*double res;
+
+    {
+        assert false;
+        double argOne = 0;
+        double argTwo = 0;
+        res = calculator.binaryOperation(argOne, argTwo, Operation.operation);
+    }*/
+
+    /*public void onButtonOneClicked() {
+    }
+
+    public void onButtonTwoClicked() {
     }
 
     public void onButtonNineClicked() {
@@ -68,9 +273,8 @@ public class CalculatorPresenter {
     }
 
     public void onButtonAddClicked() {
-        double res = calculator.binaryOperation(argOne, argTwo, Operation.ADD);
     }
 
     public void onButtonClearClicked() {
-    }
-}
+    }*/
+
--- app/src/main/java/com/android/calculator/ui/CalculatorView.java
+++ app/src/main/java/com/android/calculator/ui/CalculatorView.java
@@ -3,10 +3,4 @@ package com.android.calculator.ui;
 public interface CalculatorView {
 
     void showResult(String result);
-
-    void getSomeText();
-
-    void showProgress();
-
-    void hideProgress();
 }
